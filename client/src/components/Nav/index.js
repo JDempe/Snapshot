025410b7bb8898268930a400005b67ed6f197b17
Seer <ksmnk@protonmail.com>
2023-07-17 23:55:40 -0700

    added font, altered login/signup

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,16 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import './index.scss';
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -25,13 +24,15 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">
-              Signup
+            <Link to="/login">
+              <Button variant="outlined" className="loginButton">
+                Log In
+              </Button>
             </Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
-              Login
+            <Link to="/signup">
+              <Button className="signupButton">Sign Up</Button>
             </Link>
           </li>
         </ul>
@@ -41,18 +42,63 @@ function Nav() {
 
   return (
     <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
-        </Link>
-      </h1>
-
-      <nav>
-        {showNavigation()}
-      </nav>
+      <h1 className="websiteTitle">websiteTitle</h1>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
+
+// function Nav() {
+
+//   function showNavigation() {
+//     if (Auth.loggedIn()) {
+//       return (
+//         <ul className="flex-row">
+//           <li className="mx-1">
+//             <Link to="/orderHistory">
+//               Order History
+//             </Link>
+//           </li>
+//           <li className="mx-1">
+//             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+//             <a href="/" onClick={() => Auth.logout()}>
+//               Logout
+//             </a>
+//           </li>
+//         </ul>
+//       );
+//     } else {
+//       return (
+//         <ul className="flex-row">
+//           <li className="mx-1">
+//             <Link to="/signup">
+//               Signup
+//             </Link>
+//           </li>
+//           <li className="mx-1">
+//             <Link to="/login">
+//               Login
+//             </Link>
+//           </li>
+//         </ul>
+//       );
+//     }
+//   }
+
+//   return (
+//     <header className="flex-row px-1">
+//       <h1>
+//         <Link to="/">
+//           <span role="img" aria-label="shopping bag">🛍️</span>
+//           -Shop-Shop
+//         </Link>
+//       </h1>
+
+//       <nav>
+//         {showNavigation()}
+//       </nav>
+//     </header>
+//   );
+// }
 
 export default Nav;
