cf6d2c2320abe6c1c12690f5e653bb40731c3509
Seer <ksmnk@protonmail.com>
2023-07-18 16:46:50 -0700

    working on footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import { Facebook, Instagram, Twitter } from '@mui/icons-material';
+import { GitHub } from '@mui/icons-material';
 import { Box } from '@mui/material';
-// import './index.scss';
+import './index.scss';
 
 function Footer() {
   return (
@@ -20,45 +20,178 @@ function Footer() {
       }}>
       <Container maxWidth="lg">
         <Grid container spacing={5}>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={3}>
             <Typography variant="h6" color="text.primary" gutterBottom>
               About Us
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography className="footerDefText">
               We are XYZ company, dedicated to providing the best service to our
               customers.
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Technology
+            </Typography>
+            <div className="footerTextPadding">
+              <a
+                href="https://react.dev/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                React
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://mui.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Material UI
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://www.apollographql.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Apollo GraphQL
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://stripe.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Stripe
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://www.mongodb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                MongoDB
+              </a>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              (cont.)
+            </Typography>
+            <div className="footerTextPadding">
+              <a
+                href="https://react.dev/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                React
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://mui.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Material UI
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://www.apollographql.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Apollo GraphQL
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://stripe.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Stripe
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://www.mongodb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                MongoDB
+              </a>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={2}>
             <Typography variant="h6" color="text.primary" gutterBottom>
               Contact Us
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              123 Main Street, Anytown, USA
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Email: info@example.com
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Phone: +1 234 567 8901
-            </Typography>
+            <div className="footerTextPadding">
+              <a
+                href="https://github.com/JDempe"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                JDempe
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://github.com/jenryt"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                jenryt
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://github.com/bbandhu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                bbandhu
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://github.com/Kevrev"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                Kevrev
+              </a>
+            </div>
+            <div className="footerTextPadding">
+              <a
+                href="https://github.com/hichamraffiaidrissi"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerText">
+                hichamraffiaidrissi
+              </a>
+            </div>
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={3}>
             <Typography variant="h6" color="text.primary" gutterBottom>
               Follow Us
             </Typography>
-            <Link href="https://www.facebook.com/" color="inherit">
-              <Facebook />
-            </Link>
-            <Link
-              href="https://www.instagram.com/"
-              color="inherit"
-              sx={{ pl: 1, pr: 1 }}>
-              <Instagram />
-            </Link>
-            <Link href="https://www.twitter.com/" color="inherit">
-              <Twitter />
-            </Link>
+            <div className="footerTextPadding">
+              <a
+                href="https://github.com/JDempe/project3"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footerIcon">
+                <GitHub />
+              </a>
+            </div>
           </Grid>
         </Grid>
         <Box mt={5}>
