a2ab1afa9e013aa3c1dd0ebe4ce5ab9988bed1ea
Seer <ksmnk@protonmail.com>
2023-07-18 21:38:37 -0700

    adding individual photo page

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -13,6 +13,7 @@ import {
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
+import './Detail.scss';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -84,28 +85,33 @@ function Detail() {
   return (
     <>
       {currentProduct && cart ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
-
-          <h2>{currentProduct.name}</h2>
-
-          <p>{currentProduct.description}</p>
-
+        <div className="my-1">
+          <div className="backdrop">
+            <Link to="/">← Return</Link>
+            <div className="imageContainer">
+              <img
+                src={`/images/${currentProduct.image}`}
+                alt={currentProduct.name}
+              />
+            </div>
+          </div>
+          <div className="container my-1 imageInfo">
+            <div className="imageName">
+              <h2>{currentProduct.name}</h2>
+            </div>
+            <div className="imageDescription">
+              <p>{currentProduct.description}</p>
+            </div>
+          </div>
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
             <button
               disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
+              onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
-
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
@@ -115,3 +121,105 @@ function Detail() {
 }
 
 export default Detail;
+
+// function Detail() {
+//   const [state, dispatch] = useStoreContext();
+//   const { id } = useParams();
+
+//   const [currentProduct, setCurrentProduct] = useState({});
+
+//   const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+//   const { products, cart } = state;
+
+//   useEffect(() => {
+//     // already in global store
+//     if (products.length) {
+//       setCurrentProduct(products.find((product) => product._id === id));
+//     }
+//     // retrieved from server
+//     else if (data) {
+//       dispatch({
+//         type: UPDATE_PRODUCTS,
+//         products: data.products,
+//       });
+
+//       data.products.forEach((product) => {
+//         idbPromise('products', 'put', product);
+//       });
+//     }
+//     // get cache from idb
+//     else if (!loading) {
+//       idbPromise('products', 'get').then((indexedProducts) => {
+//         dispatch({
+//           type: UPDATE_PRODUCTS,
+//           products: indexedProducts,
+//         });
+//       });
+//     }
+//   }, [products, data, loading, dispatch, id]);
+
+//   const addToCart = () => {
+//     const itemInCart = cart.find((cartItem) => cartItem._id === id);
+//     if (itemInCart) {
+//       dispatch({
+//         type: UPDATE_CART_QUANTITY,
+//         _id: id,
+//         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+//       });
+//       idbPromise('cart', 'put', {
+//         ...itemInCart,
+//         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+//       });
+//     } else {
+//       dispatch({
+//         type: ADD_TO_CART,
+//         product: { ...currentProduct, purchaseQuantity: 1 },
+//       });
+//       idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+//     }
+//   };
+
+//   const removeFromCart = () => {
+//     dispatch({
+//       type: REMOVE_FROM_CART,
+//       _id: currentProduct._id,
+//     });
+
+//     idbPromise('cart', 'delete', { ...currentProduct });
+//   };
+
+//   return (
+//     <>
+//       {currentProduct && cart ? (
+//         <div className="container my-1">
+//           <Link to="/">← Back to Products</Link>
+
+//           <h2>{currentProduct.name}</h2>
+
+//           <p>{currentProduct.description}</p>
+
+//           <p>
+//             <strong>Price:</strong>${currentProduct.price}{' '}
+//             <button onClick={addToCart}>Add to Cart</button>
+//             <button
+//               disabled={!cart.find((p) => p._id === currentProduct._id)}
+//               onClick={removeFromCart}
+//             >
+//               Remove from Cart
+//             </button>
+//           </p>
+
+//           <img
+//             src={`/images/${currentProduct.image}`}
+//             alt={currentProduct.name}
+//           />
+//         </div>
+//       ) : null}
+//       {loading ? <img src={spinner} alt="loading" /> : null}
+//       <Cart />
+//     </>
+//   );
+// }
+
+// export default Detail;
