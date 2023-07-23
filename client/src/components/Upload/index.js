6ef351a222cd16de1ff7e201913751dc4d085a0d
JDempe <123279032+JDempe@users.noreply.github.com>
2023-07-22 23:49:46 -0400

    Added Upload modal styling

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -3,10 +3,18 @@ import { useMutation } from '@apollo/client';
 import { UPLOAD_PHOTO } from '../../utils/mutations';
 import { Alert, Collapse, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-import { Box } from '@mui/material';
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  InputLabel,
+  Input,
+  Button,
+} from '@mui/material';
+import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
 import { useLockBodyScroll } from '@uidotdev/usehooks';
+import './style.scss';
 
 function Upload() {
   useLockBodyScroll();
@@ -21,6 +29,7 @@ function Upload() {
   const [imagePreview, setImagePreview] = useState(null); // this is the image preview that will be displayed on the page
   const [isLoading, setIsLoading] = useState(null);
   const [isReady, setIsReady] = useState(false); // this is if the image is ready to be uploaded
+  const [submitDisabled, setSubmitDisabled] = useState(true); // this is if the submit button is disabled or not
   const [success, setSuccess] = useState(false); // this is the success message that will be displayed on the page
   const [failed, setFailed] = useState(false); // this is the error message that will be displayed on the page
 
@@ -30,10 +39,11 @@ function Upload() {
     setPhotoImage(event.target.files[0]);
 
     if (!event.target.files[0]) {
+      setImagePreview(null);
       return;
     }
     setImagePreview(URL.createObjectURL(event.target.files[0]));
-    setIsReady(true);
+    handleFormReady();
   };
 
   const handleFormSubmit = async (event) => {
@@ -73,39 +83,58 @@ function Upload() {
     }
   };
 
+  const handleFormReady = () => {
+    // check if the form is ready to be submitted by checking the length of the 3 fields (photoName, description, and photoImage)
+    if (
+      formState.photoName.length > 0 &&
+      formState.description.length > 0 &&
+      photoImage
+    ) {
+      setIsReady(true);
+    } else {
+      setIsReady(false);
+    }
+  };
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
       ...formState,
       [name]: value,
     });
+
+    handleFormReady();
   };
 
-  // TODO move to imported css file
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  const handleChangeTitle = (e) => {
+    let titleValid = e.target.value ? true : false; // basic title validation
+    let submitValid = this.state.textValid && titleValid; // validate total form
+    this.setState({
+      title: e.target.value,
+      titleValid: titleValid,
+      submitDisabled: !submitValid,
+    });
   };
 
   return (
     <div id="uploadModal" className="modalDiv">
       <div className="modal">
-        <Box sx={style}>
-          <button type="button" onClick={() => navigate(-1)}>
-            Close
-          </button>
-          <h2>Upload</h2>
-          <ValidatorForm onSubmit={handleFormSubmit}>
-            {/* Upload photo Button */}
-            <div>
-              <label htmlFor="upload">Upload Photo:</label>
+        <div className="modalBody">
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="medium"
+            className="closeButton"
+            onClick={() => {
+              navigate(-1);
+            }}>
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+
+          <form id="uploadForm" onSubmit={handleFormSubmit}>
+            <h2>Upload</h2>
+            <FormControl height="100%" fullWidth>
+              {/* Upload photo Button */}
               <input
                 placeholder="Upload Photo"
                 name="upload"
@@ -113,63 +142,50 @@ function Upload() {
                 id="upload"
                 onChange={handleFileUpload}
               />
-            </div>
-            <div height="300" width="300">
-              {imagePreview && <img src={imagePreview} alt="Uploaded Image" />}
-            </div>
 
-            {/* Photo Name Box */}
-            <div className="flex-row space-between my-2">
-              <label htmlFor="photoname">Photo Name:</label>
-              <TextValidator
-                label="Title"
-                type="text"
+              <div id="previewImageBox">
+                {imagePreview && (
+                  <img id="previewImage" src={imagePreview} alt="" />
+                )}
+              </div>
+
+              {/* Photo Name Box */}
+              <TextField
+                placeholder="Name your photo..."
+                id="uploadTitle"
                 name="photoName"
-                id="photoName"
+                label="Title"
+                required
+                margin="normal"
+                color="primary"
                 value={formState.photoName}
-                validators={['required', 'minStringLength:5']}
-                errorMessages={[
-                  'this field is required',
-                  'minimum 5 characters',
-                ]}
-                onChange={handleChange}
+                onChange={handleChangeTitle}
               />
-            </div>
-            {/* Photo description Textbox */}
-            <div className="flex-row space-between my-2">
-              <label htmlFor="description">Description:</label>
-              <TextValidator
+
+              {/* Photo description Textbox */}
+
+              <TextField
                 placeholder="Describe the photo..."
                 label="Description"
                 name="description"
-                type="textarea"
                 multiline
                 rows={4}
-                id="description"
+                required
+                margin="normal"
+                error={formState.description.length < 5}
+                id="uploadDescription"
                 value={formState.description}
-                validators={['required', 'minStringLength:5']}
-                errorMessages={[
-                  'this field is required',
-                  'minimum 5 characters',
-                ]}
                 onChange={handleChange}
               />
-            </div>
-            {error ? (
-              <div>
-                <p className="error-text">
-                  The provided credentials are incorrect
-                </p>
-              </div>
-            ) : null}
-            <div className="flex-row flex-end">
+
               {/* submit button that is disabled if isLoading is true */}
-              <button
-                className="btn btn-block btn-primary"
-                disabled={isLoading || !isReady}
+              <Button
+                variant="contained"
+                id="uploadButton"
+                // disabled={isLoading || !isReady}
                 type="submit">
-                {isLoading ? 'Loading...' : 'Submit'}
-              </button>
+                {isLoading ? 'Loading...' : 'Upload Photo'}
+              </Button>
 
               <Collapse in={success}>
                 <Alert
@@ -207,9 +223,9 @@ function Upload() {
                   Something went wrong! Please try again.
                 </Alert>
               </Collapse>
-            </div>
-          </ValidatorForm>
-        </Box>
+            </FormControl>
+          </form>
+        </div>
       </div>
     </div>
   );
