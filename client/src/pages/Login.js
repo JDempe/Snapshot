411b786fa01d3831b3029dd502147a32173952d8
JDempe <123279032+JDempe@users.noreply.github.com>
2023-07-23 23:44:40 -0400

    Updated Login Page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,30 +1,43 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
-import {
-  styled,
-  Switch,
-  FormGroup,
-  FormControlLabel,
-  Typography,
-} from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { styled, Switch, FormGroup } from '@mui/material';
+import { Alert, Collapse, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
+  const [failed, setFailed] = useState(false); // this is the error message that will be displayed on the page
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     try {
+      console.log(formState.email);
+      console.log(formState.password);
       const mutationResponse = await login({
         variables: { email: formState.email, password: formState.password },
       });
       const token = mutationResponse.data.login.token;
+
+      console.log(token);
       Auth.login(token);
     } catch (e) {
       console.log(e);
+      setFailed(true);
     }
   };
 
@@ -104,82 +117,120 @@ function Login(props) {
   }));
 
   return (
-    <div className="container my-1">
-      <div
-        className="w-50"
-        style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
+    <Container component="main" maxWidth="s">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
         }}>
-        {/* <Link to="/signup">← Go to Signup</Link> */}
-
-        <FormGroup aria-label="auth toggle" style={{ width: '11vw' }}>
-          <FormControlLabel
-            control={
-              <>
-                <Typography variant="body2"></Typography>
-                <Link to="/signup">
-                  <AntSwitch
-                    defaultChecked
-                    checked={mode === 'signup'}
-                    onChange={handleModeChange}
-                    color="primary"
-                    inputProps={{ 'aria-label': 'auth toggle' }}
-                  />
-                </Link>
-                <Typography variant="body2"></Typography>
-              </>
-            }
-            label=""
-            labelPlacement="start"
-            style={{
-              width: '100%',
-              justifyContent: 'space-between',
-              margin: '0',
-              flexDirection: 'column',
-            }}
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleFormSubmit}
+          noValidate
+          sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={handleChange}
           />
-        </FormGroup>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleChange}
+          />
+          {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+          <Collapse in={failed}>
+            <Alert
+              severity="error"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setFailed(false);
+                  }}>
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+              sx={{ mb: 2 }}>
+              Incorrect Credentials! Please try again.
+            </Alert>
+          </Collapse>
+          <Grid container>
+            <Grid item xs>
+              {/* <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link> */}
+              <FormGroup aria-label="auth toggle" style={{ width: '11vw' }}>
+                <FormControlLabel
+                  control={
+                    <>
+                      <Typography variant="body2"></Typography>
+                      <Link to="/signup">
+                        <AntSwitch
+                          defaultChecked
+                          checked={mode === 'signup'}
+                          onChange={handleModeChange}
+                          color="primary"
+                          inputProps={{ 'aria-label': 'auth toggle' }}
+                        />
+                      </Link>
+                      <Typography variant="body2"></Typography>
+                    </>
+                  }
+                  label=""
+                  labelPlacement="start"
+                  style={{
+                    width: '100%',
+                    justifyContent: 'space-between',
+                    margin: '0',
+                    flexDirection: 'column',
+                  }}
+                />
+              </FormGroup>
+            </Grid>
 
-        <h2>Login</h2>
-        <p>No account? Switch to sign up!</p>
-        <form onSubmit={handleFormSubmit}>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="email">Email address:</label>
-            <input
-              placeholder="youremail@test.com"
-              name="email"
-              type="email"
-              id="email"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="pwd">Password:</label>
-            <input
-              placeholder="******"
-              name="password"
-              type="password"
-              id="pwd"
-              onChange={handleChange}
-            />
-          </div>
-          {error ? (
-            <div>
-              <p className="error-text">
-                The provided credentials are incorrect
-              </p>
-            </div>
-          ) : null}
-          <div className="flex-row" style={{ justifyContent: 'center' }}>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    </div>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
