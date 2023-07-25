ee434bb2055d5c735d9a42e96581498173ce4f8f
Seer <ksmnk@protonmail.com>
2023-07-25 09:15:55 -0700

    adding icons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import ShareIcon from '@mui/icons-material/Share';
+import SellIcon from '@mui/icons-material/Sell';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
+import PublicIcon from '@mui/icons-material/Public';
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Home.scss';
 
 function Home() {
@@ -14,8 +23,38 @@ function Home() {
               <div className="blurbText">
                 Start sharing your pictures with the world around you.
               </div>
+              <Link to="/signup">
+                <Button className="signupButtonHome">Sign Up</Button>
+              </Link>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="homeContainer">
+        <div className="pitchContainer">
+          <PublicIcon className="pitchIcon" />
+          <ShareIcon className="pitchIconSecond" />
+          <div className="pitchTitle">Share with the world</div>
+          <div className="pitchText">This is the text</div>
+        </div>
+        <div className="pitchContainer">
+          <AddReactionIcon className="pitchIcon" />
+          <FavoriteBorderIcon className="pitchIconSecond" />
+          <div className="pitchTitle">Find your favorites</div>
+          <div className="pitchText">This is the text</div>
+        </div>
+        <div className="pitchContainer">
+          <ShoppingCartIcon className="pitchIcon" />
+          <PriceCheckIcon className="pitchIconSecond" />
+
+          <div className="pitchTitle">Like it? Buy it</div>
+          <div className="pitchText">This is the text</div>
+        </div>
+        <div className="pitchContainer">
+          <SellIcon className="pitchIcon" />
+          <MonetizationOnIcon className="pitchIconSecond" />
+          <div className="pitchTitle">This is the title</div>
+          <div className="pitchText">This is the text</div>
         </div>
       </div>
     </div>
