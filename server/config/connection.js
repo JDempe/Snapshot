298e5a47c98d2976375cccfb4654fc152ad769df
JDempe <123279032+JDempe@users.noreply.github.com>
2023-07-26 00:15:28 -0400

    Updated database names

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/snapshot-marketplace'
+);
 
 module.exports = mongoose.connection;
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Size = require('./Size');
-// const Category = require('./Category');
 const Order = require('./Order');
 const Photo = require('./Photo');
 const Comment = require('./Comment');
